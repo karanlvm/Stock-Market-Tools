5a77ba0a1df1d86f83a491e0ff3fdf8c8fe0b057
Karan <karanlvm123@gmail.com>
2021-09-07 21:41:25 +0530

    2 new tools
    Added a candlestick graph creator and a tool that predicts the closing price of the stock.

---
+++ candlestick.py
@@ -0,0 +1,38 @@
+import datetime as dt
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from mpl_finance import candlestick_ohlc
+
+# Defining the time frame
+
+start = dt.datetime(2021,1,1)
+end = dt.datetime.now()
+
+# Load the data
+
+data = web.DataReader('AAPL', 'yahoo', start, end)
+
+# Data Formatting
+
+data = data[['Open', 'High', 'Low', 'Close']]
+
+data.reset_index(inplace=True)
+data['Date'] = data['Date'].map(mdates.date2num)
+
+print(data.head())
+
+# Plotting the data
+
+ax = plt.subplot()
+ax.grid(True)
+ax.set_axisbelow(True)
+ax.set_title('Share Price', color='white')
+ax.set_facecolor('black')
+ax.figure.set_facecolor('#121212')
+ax.tick_params(axis='x', colors='white')
+ax.tick_params(axis='y', colors='white')
+ax.xaxis_date()
+
+candlestick_ohlc(ax, data.values, width=0.5, colorup='#00ff00')
+plt.show()
